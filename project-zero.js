3dc479068e0098aa24960d7266c3d32a5805f97a
Olivia Sarsfield <160706207+LadyVanir@users.noreply.github.com>
2025-03-16 23:32:05 -0400

    1

--- project-zero.js
+++ project-zero.js
@@ -12,7 +12,8 @@ import { I18NMixin } from "@haxtheweb/i18n-manager/lib/I18NMixin.js";
  * @demo index.html
  * @element project-zero
  */
-export class ProjectZero extends DDDSuper(I18NMixin(LitElement)) {
+export class ProjectZero extends DDDSuper(I18NMixin(LitElement)) 
+{
 
   static get tag() {
     return "project-zero";
@@ -63,21 +64,63 @@ export class ProjectZero extends DDDSuper(I18NMixin(LitElement)) {
     `];
   }
 
+  async fetchContributors()
+  {
+    const url = 'https://api.github.com/repos/olivia-sarsfield/project-zero/contributors';
+    try
+    {
+      const response = await fetch(url);
+      if(!response.ok)
+      {
+        throw new Error('Network response was not ok');
+      }
+      const data = await response.json();
+      this.contributors = data.slice(0, this.limit)
+
+    }
+    catch(error)
+    {
+      console.error('There was a problem with your fetch operation:', error);
+    }
+  }
+
   // Lit render the HTML
-  render() {
+  render() 
+  {
     return html`
 <div class="wrapper">
   <h3><span>${this.t.title}:</span> ${this.title}</h3>
+  ${this.contributors.length > 0
+    ? html`
+        ${this.contributors.map(
+        (contributor) => html`
+         <div class="contributor">
+         <img src="${contributor.avatar_url}" alt="${contributor.login}" />
+         <div class="contributor-info">
+          <a
+           href="${contributor.html_url}"
+              target="_blank"
+              title="Visit ${contributor.login}'s GitHub"
+             class="contributor-name" >
+                  ${contributor.login}
+                </a>
+                <p>Contributions: ${contributor.contributions}</p>
+              </div>
+            </div>
+          `
+        )}
+      `
+    : html`<p>Loading contributors...</p>`}
   <slot></slot>
-</div>`;
+</div>
+    `;
   }
 
   /**
    * haxProperties integration via file reference
    */
   static get haxProperties() {
-    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url)
-      .href;
+    return new URL(`./lib/${this.tag}.haxProperties.json`, import.meta.url).href;
   }
 }
 
